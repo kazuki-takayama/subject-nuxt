e33098e2dfca752f305bf1d72c46b45ee44e8784
kazuki-takayama <kazu6657714@gmail.com>
2021-11-11 15:18:57 +0900

    削除ボタン追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,87 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
+  <v-container>
+    <v-row>
+      <v-col cols="10">
+        <v-data-table
+          :headers="headers"
+          :items="items"
+        >
+        <template v-slot:item.delete="{ item }">
           <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
+            small
+            color="error"
+            @click="deleteItem(item)"
           >
-            Continue
+            delete
           </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+        </template>
+        </v-data-table>
+
+      </v-col>
+      </v-row>
+  </v-container>
 </template>
+
+
+<script>
+export default {
+
+  data(){
+    return {
+      headers: [
+          { 
+            text: 'Todo', 
+            value: 'id' 
+          },
+          {
+            text: '予定',
+            value: 'name',
+          },
+          { text: '完了', 
+            value: 'name' 
+          },
+          {
+            text:'削除',
+            value:'delete',
+            sortable:false
+          }
+        ],
+        serverDatas:[
+
+      ],
+
+      items:[
+        { 
+          Todo : 1 , 
+          name : "" , 
+          name : ""
+        },
+        { 
+          予定 : 2 , 
+          name : "" , 
+          name: ""
+        },
+        { 
+          完了 : 3 , 
+          name : "資料作成" , 
+          name: "資料作成"
+        },
+      ],
+      
+
+    }
+  },
+
+  methods: {
+　　deleteItem (item) {
+
+    const index = this.serverDatas.indexOf(item)
+  　  confirm('削除しますか') && this.items.splice(index, 1)
+  
+    
+　 　  },
+
+  },
+
+}
+　</script>
