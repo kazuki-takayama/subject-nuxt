2f60ef32d6238c415cb0f96f811870d483d74b9b
kazuki-takayama <kazu6657714@gmail.com>
2021-11-18 15:17:18 +0900

    axios

---
+++ client/pages/index copy.vue
@@ -0,0 +1,27 @@
+  <template>
+	<div>
+		<ul>
+		<li v-for="(post, index) in posts" :key="index">
+  <a :href="'post.url'" target="_blank" rel="noopener noreferrer">{{ post.title }}</a>
+			</li>
+		</ul>
+	</div>
+</template>
+  
+  
+  
+  <script>
+import axios from 'axios';
+
+export default {
+  data(){
+    return {
+      posts: []
+    }
+  },
+  mounted(){
+    axios.get('http://localhost:4000')
+          .then(response => this.posts = response.data);
+  }
+}
+</script>
