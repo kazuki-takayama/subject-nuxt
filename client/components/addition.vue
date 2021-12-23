fbf451bef2c97930fc8eea7bf7fdbc0250fcf51e
kazuki-takayama <kazu6657714@gmail.com>
2021-12-23 15:29:27 +0900

    components

--- client/components/addition.vue
+++ client/components/addition.vue
@@ -1,5 +1,4 @@
-<template>
-  
+    <template>
         <v-dialog v-model="dialog" max-width="500px">
             <template v-slot:activator="{ on, attrs }">
               <v-btn color="primary" dark class="mb-2"
@@ -54,9 +53,29 @@
 <script>
 import axios from 'axios';
 export default {
+
   asyncData() {
     return {
       posts: [],
+      headers: [
+        {
+          text: 'Todo',
+          value: 'id',
+        },
+        {
+          text: '予定',
+          value: 'name',
+        },
+        {
+          text: '完了',
+          value: 'task',
+        },
+        {
+          text: '削除',
+          value: 'delete',
+          sortable: false
+        }
+      ],
       dialog: false,
       dialogDelete: false,
       editedIndex: -1,
@@ -73,20 +92,40 @@ export default {
       ],
     }
   },
-
+  
   mounted() {
     axios.get('http://localhost:4000')
     .then(response => this.items = response.data);
   },
   methods: {
-  close: function(){
+  close () {
+      this.dialog = false
+      this.$nextTick(() => {
+      this.editedItem = Object.assign({}, this.defaultItem)
+      this.editedIndex = -1
+      })
   this.$emit('close');
   },
 
-    create: function(){
+    create () {
+      this.items.push(this.editedItem)
+    axios.post('http://localhost:4000/add',this.editedItem)
+    .then(response => {
+        console.log('response :editedItem', response.item);
+    });
     this.$emit('create');
+    },
+
+    remove(item) {
+    console.log(item._id)
+    console.log(item)
+    axios.delete(`http://localhost:4000/todos/${item._id}`).then(res => {    
+      this.editedIndex = this.items.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.items.splice(this.editedIndex, 1)
+      this.dialogDelete = true
+    })
     }
-    
   },
 }
 </script>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,8 +2,8 @@
   <v-container>
     <v-row>
       <v-col cols="10">
-        <addition　@click="close"> </addition>
-        <addition　@click="create"> </addition>
+        <addition>
+          </addition>
         <v-data-table :headers="headers" :items="items">
           <template v-slot:item.delete="{ item }">
             <v-btn small color="error" @click="remove(item)">delete</v-btn>
@@ -17,46 +17,13 @@
 </template>
 
 <script>
-import addition from './components/addition.vue'
-import axios from 'axios';
-export default {
+  import addition from '~/components/addition.vue'
+  export default {
   components: {
     addition,
-  },
-
-  mounted() {
-    axios.get('http://localhost:4000')
-    .then(response => this.items = response.data);
-  },
-  methods: {
-      
-    close () {
-      this.dialog = false
-      this.$nextTick(() => {
-      this.editedItem = Object.assign({}, this.defaultItem)
-      this.editedIndex = -1
-      })
-    },
-    
-    create () {
-      this.items.push(this.editedItem)
-    axios.post('http://localhost:4000/add',this.editedItem)
-    .then(response => {
-        console.log('response :editedItem', response.item);
-    });
-    },
-    
-    remove(item) {
-    console.log(item._id)
-    console.log(item)
-    axios.delete(`http://localhost:4000/todos/${item._id}`).then(res => {    
-      this.editedIndex = this.items.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.items.splice(this.editedIndex, 1)
-      this.dialogDelete = true
-    })
-    }
-  },
+  }
 }
+
+  
 </script>
 
