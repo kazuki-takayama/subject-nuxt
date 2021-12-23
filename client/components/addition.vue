2cbb2b60e873f8663b2e30dd48e98f0904b93fca
kazuki-takayama <kazu6657714@gmail.com>
2021-12-23 11:52:26 +0900

    コンポーネント

---
+++ client/components/addition.vue
@@ -0,0 +1,93 @@
+<template>
+  
+        <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="primary" dark class="mb-2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                TODO追加
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+              </v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.name"
+                        label="todo"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+              
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="close"
+                >
+                  Cancel
+                </v-btn>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="create"
+                >
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  asyncData() {
+    return {
+      posts: [],
+      dialog: false,
+      dialogDelete: false,
+      editedIndex: -1,
+      defaultItem: {
+      name: '',
+    },
+    editedItem: {
+      name: '',
+    },
+      serverDatas: [
+      ],
+      items: [
+        
+      ],
+    }
+  },
+
+  mounted() {
+    axios.get('http://localhost:4000')
+    .then(response => this.items = response.data);
+  },
+  methods: {
+  close: function(){
+  this.$emit('close');
+  },
+
+    create: function(){
+    this.$emit('create');
+    }
+    
+  },
+}
+</script>
+
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,61 +1,9 @@
 <template>
   <v-container>
     <v-row>
-      
       <v-col cols="10">
-        <v-dialog
-            v-model="dialog"
-            max-width="500px">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                color="primary"
-                dark
-                class="mb-2"
-                v-bind="attrs"
-                v-on="on"
-              >
-                TODO追加
-              </v-btn>
-            </template>
-            <v-card>
-              <v-card-title>
-              </v-card-title>
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                    >
-                      <v-text-field
-                        v-model="editedItem.name"
-                        label="todo"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-              
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="close"
-                >
-                  Cancel
-                </v-btn>
-                <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="create"
-                >
-                  Save
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
+        <addition　@click="close"> </addition>
+        <addition　@click="create"> </addition>
         <v-data-table :headers="headers" :items="items">
           <template v-slot:item.delete="{ item }">
             <v-btn small color="error" @click="remove(item)">delete</v-btn>
@@ -69,46 +17,11 @@
 </template>
 
 <script>
+import addition from './components/addition.vue'
 import axios from 'axios';
 export default {
-
-  asyncData() {
-    return {
-      posts: [],
-      headers: [
-        {
-          text: 'Todo',
-          value: 'id',
-        },
-        {
-          text: '予定',
-          value: 'name',
-        },
-        {
-          text: '完了',
-          value: 'task',
-        },
-        {
-          text: '削除',
-          value: 'delete',
-          sortable: false
-        }
-      ],
-      dialog: false,
-      dialogDelete: false,
-      editedIndex: -1,
-      defaultItem: {
-      name: '',
-    },
-    editedItem: {
-      name: '',
-    },
-      serverDatas: [
-      ],
-      items: [
-        
-      ],
-    }
+  components: {
+    addition,
   },
 
   mounted() {
