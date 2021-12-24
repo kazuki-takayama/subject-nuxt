e3b8a3cb5a0697083f92245b95bad0f3790beddf
kazuki-takayama <kazu6657714@gmail.com>
2021-12-25 00:20:58 +0900

    Composition API

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -30,7 +30,35 @@ import { defineComponent, reactive, ref } from '@nuxtjs/composition-api'
   },
   
     setup(items)  {
-    return {
+
+　const add = () => {
+　  this.editedItem = false
+  items.push( this.editedItem );
+　}
+
+    },
+
+  onMounted (()=>{
+    axios.get('http://localhost:4000')
+    then.(response => this.items = response.data);
+  })
+
+  
+  remove(item) {
+    console.log(item._id)
+    console.log(item)
+    axios.delete(`http://localhost:4000/todos/${item._id}`).then(res => {    
+      this.editedIndex = this.items.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.items.splice(this.editedIndex, 1)
+      this.dialogDelete = true
+    })
+    }
+    
+  
+
+
+  return {
       posts: [],
       headers: [
         {
@@ -54,46 +82,23 @@ import { defineComponent, reactive, ref } from '@nuxtjs/composition-api'
       defaultItem: {
         name: '',
       },
-      
+      add,
+  
     
-　add: function(){
-　  this.editedItem = false
-  items.push( this.editedItem );
-　},
 
-
-      defaultItem: {
-      name: '',
-    },
-    editedItem: {
-      name: '',
-    },
-      serverDatas: [
-      ],
-      items: [
+    //   //defaultItem: {
+    //   //name: '',
+    // //},
+    // //editedItem: {
+    // //  name: '',
+    // //},
+    //   //serverDatas: [
+    //   //],
+    //   //items: [
         
-      ],
-    }
-    },
-  
-  getRate(){
-    axios.get('http://localhost:4000')
-    then(response => this.items = response.data);
-  },
-
-  
-  remove(item) {
-    console.log(item._id)
-    console.log(item)
-    axios.delete(`http://localhost:4000/todos/${item._id}`).then(res => {    
-      this.editedIndex = this.items.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.items.splice(this.editedIndex, 1)
-      this.dialogDelete = true
-    })
-    }
-    
-  })
+    //   //],
+    // }
+    };
 
 
   
