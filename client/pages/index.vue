2d60e38165e35c81bd820c97817ff74d408a2a8c
kazuki-takayama <kazu6657714@gmail.com>
2021-12-25 02:21:03 +0900

    コンポーネンんと

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,17 +1,14 @@
 <template>
   <v-container>
     <v-row>
-      
       <v-col cols="10">
-
         <addition @add="add"></addition>
 
         <v-data-table :headers="headers" :items="items">
           <template v-slot:item.delete="{ item }">
             <v-btn small color="error" @click="remove(item)">delete</v-btn>
           </template>
-          <template v-slot:item.actions="{ item }">
-    </template>
+          <template v-slot:item.actions="{ item }"> </template>
         </v-data-table>
       </v-col>
     </v-row>
@@ -29,76 +26,78 @@ import { defineComponent, reactive, ref } from '@nuxtjs/composition-api'
     addition,
   },
   
-    setup(items)  {
+    setup()  {
 
 　const add = () => {
-　  this.editedItem = false
-  items.push( this.editedItem );
+　  editedItem = false
+  items.push( editedItem.name );
 　}
+const headers = reactive ({
+  headers:[
+  {text: 'Todo',value: 'id'},
+  {text: '予定',value: 'name'},
+  {text: '完了',value: 'task'},
+  {text: '削除',value: 'delete',sortable: false},
+  ]
+})
+const items = reactive ({
+  items:[
+    {sss:sssddddfffggg}
+  ]
+})
 
-    },
+console.log(items.items)
+const editedItem = reactive ({
+  name: ''
+})
 
-  onMounted (()=>{
+const defaultItem = reactive ({
+  name: ''
+})
+
+const serverDatas = reactive ({
+  items:[]
+
+})
+
+
+    
+
+  onMounted (() => {
     axios.get('http://localhost:4000')
-    then.(response => this.items = response.data);
-  })
-
+    then,(response => items = response.data);
   
-  remove(item) {
+  
+  remove(item) ;{
     console.log(item._id)
     console.log(item)
     axios.delete(`http://localhost:4000/todos/${item._id}`).then(res => {    
-      this.editedIndex = this.items.indexOf(item)
+      this.editedIndex = items.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.items.splice(this.editedIndex, 1)
       this.dialogDelete = true
     })
-    }
     
-  
+  }
+    })
 
+  
 
   return {
       posts: [],
-      headers: [
-        {
-          text: 'Todo',
-          value: 'id',
-        },
-        {
-          text: '予定',
-          value: 'name',
-        },
-        {
-          text: '完了',
-          value: 'task',
-        },
-        {
-          text: '削除',
-          value: 'delete',
-          sortable: false
-        }
-      ],
-      defaultItem: {
-        name: '',
-      },
+      defaultItem,
+      headers,
       add,
+    editedItem,
   
-    
+    serverDatas,
+    items,
 
-    //   //defaultItem: {
-    //   //name: '',
-    // //},
-    // //editedItem: {
-    // //  name: '',
-    // //},
-    //   //serverDatas: [
-    //   //],
-    //   //items: [
-        
-    //   //],
-    // }
-    };
+  }
+  }
+    
+    })
+    
 
 
   
